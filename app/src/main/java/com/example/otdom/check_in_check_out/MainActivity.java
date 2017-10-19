2886adf013e9a264769975656c015a0a77e1f83b
Dibee <pensereyotdom@gmail.com>
2017-10-19 19:30:17 +0700

    getdata from database

--- app/src/main/java/com/example/otdom/check_in_check_out/MainActivity.java
+++ app/src/main/java/com/example/otdom/check_in_check_out/MainActivity.java
@@ -1,26 +1,41 @@
 package com.example.otdom.check_in_check_out;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.ImageView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 
 public class MainActivity extends AppCompatActivity {
 
-
-
+    Button gen_btn;
+    ImageView image;
+    String JSON_String;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        gen_btn = (Button) findViewById(R.id.gen_btn);
+        image = (ImageView) findViewById(R.id.image);
+        gen_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, QR_Key.class);
+                startActivity(intent);
+            }
+        });
     }
 
-    public void btn1(View view) {
-        Intent intent = new Intent(MainActivity.this,QR_Key.class);
-        startActivity(intent);
-    }
 }
--- app/src/main/java/com/example/otdom/check_in_check_out/QR_Key.java
+++ app/src/main/java/com/example/otdom/check_in_check_out/QR_Key.java
@@ -1,33 +1,119 @@
 package com.example.otdom.check_in_check_out;
 
-import android.content.Intent;
+
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.TextView;
+
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class QR_Key extends AppCompatActivity {
+    ImageView image;
+    String text2Qr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qr__key);
 
-
+    // Toolbar
         Toolbar toolbarTop = (Toolbar) findViewById(R.id.toolbar_top);
         toolbarTop.setTitleTextColor(ContextCompat.getColor(this, R.color.colorWhite));
-
 
         setSupportActionBar(toolbarTop);
         getSupportActionBar().setTitle("QR Key");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    // Generate qr_code
+        image = (ImageView) findViewById(R.id.image);
+
+        getJSON("http://10.10.11.3/MyWebservice/api/getdata.php");
 
     }
 
+
+
+    private void getJSON(final String urlWebService) {
+
+        class GetJSON extends AsyncTask<Void, Void, String> {
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+            }
+
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                Log.d("======DATA====", s);
+                generateQRData(s);
+            }
+
+
+
+            @Override
+            protected String doInBackground(Void... voids) {
+                try {
+                    URL url = new URL(urlWebService);
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                    StringBuilder sb = new StringBuilder();
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                    String json;
+                    while ((json = bufferedReader.readLine()) != null) {
+                        sb.append(json + "\n");
+                    }
+                    return sb.toString().trim();
+                } catch (Exception e) {
+                    return null;
+                }
+            }
+        }
+        GetJSON getJSON = new GetJSON();
+        getJSON.execute();
+    }
+
+    public void generateQRData(String json){
+        JSONArray jsonArray = null;
+        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+        String username;
+        try {
+            jsonArray = new JSONArray(json);
+            JSONObject obj = jsonArray.getJSONObject(0);
+            username = obj.getString("name");
+            BitMatrix bitMatrix = multiFormatWriter.encode(username, BarcodeFormat.QR_CODE, 150, 150);
+            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+            Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+            image.setImageBitmap(bitmap);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (WriterException e){
+            e.printStackTrace();
+        }
+
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
