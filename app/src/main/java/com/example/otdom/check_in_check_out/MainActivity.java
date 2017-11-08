155429802663b8409bd273c41576014c07cbf3eb
Dibee <pensereyotdom@gmail.com>
2017-11-08 16:04:37 +0700

    qr

--- app/src/main/java/com/example/otdom/check_in_check_out/MainActivity.java
+++ app/src/main/java/com/example/otdom/check_in_check_out/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.otdom.check_in_check_out;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import java.io.BufferedReader;
@@ -21,21 +25,34 @@ public class MainActivity extends AppCompatActivity {
 
     Button gen_btn;
     ImageView image;
-    String JSON_String;
+    EditText text;
+    String number;
+    private static SharedPreferences sharedPreferences;
+    private static SharedPreferences.Editor editor;
+    // int val;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        text = (EditText) findViewById(R.id.text);
         gen_btn = (Button) findViewById(R.id.gen_btn);
         image = (ImageView) findViewById(R.id.image);
         gen_btn.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
+                number= text.getText().toString().trim();
+                Log.v("=============--------", number);
+                sharedPreferences = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+                editor = sharedPreferences.edit();
+                editor.putString("number",number);
+                editor.apply();
                 Intent intent = new Intent(MainActivity.this, QR_Key.class);
                 startActivity(intent);
             }
         });
+
+
     }
 
     public void gen_btn(View view) {
--- app/src/main/java/com/example/otdom/check_in_check_out/QR_Key.java
+++ app/src/main/java/com/example/otdom/check_in_check_out/QR_Key.java
@@ -1,8 +1,10 @@
 package com.example.otdom.check_in_check_out;
 
 
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.os.AsyncTask;
+import android.preference.PreferenceManager;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,6 +13,7 @@ import android.util.Log;
 import android.view.MenuItem;
 
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -32,22 +35,35 @@ import java.net.URL;
 
 public class QR_Key extends AppCompatActivity {
     ImageView image;
+    EditText text;
+    int  val;
+    SharedPreferences prefs;
+    String text2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qr__key);
-
-    // Toolbar
+        prefs = PreferenceManager.getDefaultSharedPreferences(QR_Key.this);
+        val = Integer.parseInt(prefs.getString("number",null));
+        //val= String.valueOf(Integer.parseInt(val));
+        Log.v("________________", String.valueOf(val));
+        // Toolbar
         Toolbar toolbarTop = (Toolbar) findViewById(R.id.toolbar_top);
         toolbarTop.setTitleTextColor(ContextCompat.getColor(this, R.color.colorWhite));
 
         setSupportActionBar(toolbarTop);
         getSupportActionBar().setTitle("QR Key");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-    // Generate qr_code
+        // Generate qr_code
+        text = (EditText) findViewById(R.id.text);
+//        Log.d("=======dddd", String.valueOf(text));
+////        text2 = text.getText().toString().trim();
+//        Log.d("===========", String.valueOf(text2));
+//        val = Integer.parseInt( text2);
+//        Log.d("=======dd", String.valueOf(val));
         image = (ImageView) findViewById(R.id.image);
-        getJSON("http://10.10.18.28/MyWebservice/api/getdata.php");
+        getJSON("http://10.10.11.3/MyWebservice/api/getdata.php");
 
     }
 
@@ -63,7 +79,7 @@ public class QR_Key extends AppCompatActivity {
             @Override
             protected void onPostExecute(String s) {
                 super.onPostExecute(s);
-//                Log.d("======DATA====", s);
+                Log.d("======DATA====", s);
                 generateQRData(s);
             }
 
@@ -91,10 +107,16 @@ public class QR_Key extends AppCompatActivity {
     public void generateQRData(String json){
         JSONArray jsonArray = null;
         MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+        // Integer.parseInt(text.getText().toString());
+        //val = Integer.parseInt( text2);
+        //Log.d("===========", String.valueOf(val));
         String username;
         try {
+
             jsonArray = new JSONArray(json);
-            JSONObject obj = jsonArray.getJSONObject(4); //change to get last string in database
+            //JSONObject obj = jsonArray.getJSONObject(jsonArray.length()-1); //change to get last string in database
+            JSONObject obj = jsonArray.getJSONObject(val-1);
+            //Log.v("++++++++++++++", String.valueOf(val));
             username = obj.getString("name");
             BitMatrix bitMatrix = multiFormatWriter.encode(username, BarcodeFormat.QR_CODE, 150, 150);
             BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
